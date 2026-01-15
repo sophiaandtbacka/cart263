72f22113bd57a9cbb9a7cd3c40f67bd9580870f1
sophiaandtbacka <145041314+sophiaandtbacka@users.noreply.github.com>
2026-01-14 19:00:11 -0500

    Finished Step 1

---
+++ task 1/task 1/js/step1.js
@@ -0,0 +1,55 @@
+"use strict";
+const ellipses = {
+    x: 100,
+    y: 200,
+    w: 50,
+    h: 25,
+    fill: {
+        r: 50,
+        g: 250,
+        b: 120,
+    }
+}
+
+function setup() {
+    console.log("go");
+    createCanvas(500, 500);
+
+}
+
+function draw() {
+    noStroke();
+
+    //ellipse 1
+    push();
+    fill(ellipses.fill.r, ellipses.fill.g, ellipses.fill.b);
+    ellipse(ellipses.x, ellipses.y, ellipses.w, ellipses.h);
+    pop();
+
+    //ellipse 2
+    push();
+    ellipses.fill.r = 30;
+    ellipses.fill.g = 30;
+    ellipses.fill.b = 30;
+    ellipses.x = 30;
+    ellipses.y = 30;
+    ellipses.w = 30;
+    ellipses.h = 30;
+    fill(ellipses.fill.r, ellipses.fill.g, ellipses.fill.b);
+    ellipse(ellipses.x, ellipses.y, ellipses.w, ellipses.h);
+    pop();
+
+    //ellipse 3
+    push();
+    ellipses.fill.r = 130;
+    ellipses.fill.g = 70;
+    ellipses.fill.b = 80;
+    ellipses.x = 350;
+    ellipses.y = 150;
+    ellipses.w = 40;
+    ellipses.h = 70;
+    fill(ellipses.fill.r, ellipses.fill.g, ellipses.fill.b);
+    ellipse(ellipses.x, ellipses.y, ellipses.w, ellipses.h);
+    pop();
+
+}
