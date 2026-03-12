721349c232197a868c1ca7ec9928b170a6fa0b7e
sophiaandtbacka <145041314+sophiaandtbacka@users.noreply.github.com>
2026-03-12 01:13:21 -0400

    Added comments

---
+++ CART263-Project1-GalleryV1/js/gallery.js
@@ -0,0 +1,53 @@
+window.onload = function () {
+    let gallery = {
+        galleryStars: [], //stars array
+        numGalleryStars: 35, //amount of stars generated
+    };
+
+    //creates all of stars on the gallery page background
+    function createGalleryStars() {
+        //create some stars
+        for (let i = 0; i < gallery.numGalleryStars; i++) {
+            //defines stars' variables
+            let x = Math.random() * window.innerWidth; //random x start position
+            let y = Math.random() * window.innerHeight; //random y start position
+            let w = Math.floor(Math.random() * 5 + 1) * 20;//makes the stars 20,40,60,80px wide
+            let h = Math.floor(Math.random() * 5 + 1) * 20;
+
+            //color of stars
+            // let tint = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;//does random color change with 100% opacity on mask tint layer
+            let tint = `rgb(${255}, ${255}, ${255})`
+
+            //creates stars
+            let star = new galleryStar(x, y, w, h, tint);
+
+            //puts info of created stars in array
+            gallery.galleryStars.push(star);//pushes bee into bees array
+        }
+    }
+
+    //draws all the stars on the gallery page background
+    function renderGalleryStars() {
+        // renders all stars, set num of stars at top in gallery
+        for (let i = 0; i < gallery.galleryStars.length; i++) {
+            let star = gallery.galleryStars[i];
+            star.renderGalleryStars();
+        }
+    }
+
+    //animates all gallery background stars    
+    function animateGalleryStars() {
+        // animates all stars, all movement in animate in galleryStars.js 
+        for (let i = 0; i < gallery.galleryStars.length; i++) {//galleryStars.length is array length
+            let star = gallery.galleryStars[i];
+            star.animateGalleryStars();
+        }
+    }
+
+    //calls all functions for Gallery Background Stars
+    createGalleryStars();
+    renderGalleryStars();
+    animateGalleryStars();
+
+
+}
---
+++ CART263-Project1-GalleryV1/js/galleryStars.js
@@ -0,0 +1,77 @@
+class galleryStar {
+    constructor(x, y, width, height, tintColor) {
+        //variables given value when calling function
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.tintColor = tintColor;
+        this.vx = this.vx;
+
+        // Make the wrapper div
+        this.body = document.createElement("div");
+    }
+
+
+    // Since I uploaded a PNG and we need to modify the color, I'll create a tint layer and mask out the transparent background
+    renderGalleryStars() {
+        // Wrapper div
+        this.body.classList.add("star");
+        this.body.style.position = "absolute";
+        this.body.style.width = this.width + "px";
+        this.body.style.height = this.height + "px";
+        this.body.style.left = this.x + "px";
+        this.body.style.top = this.y + "px";
+
+        // star image
+        let starImg = document.createElement("img");
+        starImg.src = "./media/star.png";
+        starImg.style.width = "100%";
+        starImg.style.height = "100%";
+        starImg.style.objectFit = "contain";
+        starImg.style.position = "absolute"; // Stack it inside the wrapper
+
+        // Tinted overlay, for color change if you want to use it 
+        let tintLayer = document.createElement("div");
+        tintLayer.style.width = "100%";
+        tintLayer.style.height = "100%";
+        tintLayer.style.position = "absolute"; // Stack exactly on top of the image
+
+        // Apply the color from the constructor parameter
+        tintLayer.style.backgroundColor = this.tintColor;
+
+        // Mask out the background so that only the star is colored
+        tintLayer.style.maskImage = "url('./media/star.png')";
+        tintLayer.style.maskSize = "contain";
+        tintLayer.style.maskRepeat = "no-repeat";//masks it so there isn't multiple masks
+        tintLayer.style.maskPosition = "center";//centers the mask over the image
+
+        // Blend the tint and star colors
+        tintLayer.style.mixBlendMode = "multiply";
+
+        // Put it all together
+        this.body.appendChild(starImg);
+        this.body.appendChild(tintLayer);
+        document.getElementsByTagName("main")[0].appendChild(this.body);
+    }
+
+    animateGalleryStars() {
+        //velocitiy for stars, only x change right now
+        this.vx = Math.random() * 1.1 + 0.1;
+
+        //updates x position with velocity
+        this.x += this.vx;
+
+        //update div position
+        this.body.style.left = this.x + "px";
+        this.body.style.top = this.y + "px";
+
+        // reset stars when they go off screen
+        if (this.x > window.innerWidth) {
+            this.x = -this.width;
+        }
+
+        //callback so it keeps animating
+        window.requestAnimationFrame(() => this.animateGalleryStars());
+    }
+}
