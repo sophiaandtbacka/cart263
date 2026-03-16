5c6909a868ff20d3e58c1297272a0eb3fcb0e314
sophiaandtbacka <145041314+sophiaandtbacka@users.noreply.github.com>
2026-03-15 20:02:36 -0400

    changed img assets and added new assets

--- CART263-Project1-GalleryV1/js/galleryStarFrame.js
+++ CART263-Project1-GalleryV1/js/galleryStarFrame.js
@@ -14,18 +14,18 @@ class galleryStarFrame {
 
         // white star background img layer, masks animated background stars, needs to be an img layer b/c otherwise rect div preemptively masks animated stars
         this.backgroundLayer = document.createElement("img");//makes an img element, controled by css .starFrame img
-        this.backgroundLayer.src = "./media/starFrameBackground.png";//gives the white background img location
+        this.backgroundLayer.src = "./media/starFrameBackground2.png";//gives the white background img location
         //this.backgroundLayer.style.zIndex = 1;
 
         // graffiti img layer
         this.graffitiLayer = document.createElement("img");//makes an img element, controled by css .starFrame img
-        this.graffitiLayer.src = "./media/starFrameFill.png";//gives the graffiti fill img location
+        this.graffitiLayer.src = "./media/starFrameFill2.png";//gives the graffiti fill img location
         // this.backgroundLayer.style.zIndex = 2;
 
 
         // star outline img layer
         this.outlineLayer = document.createElement("img");//makes an img element, controled by css .starFrame img
-        this.outlineLayer.src = "./media/starFrame.png";//gives the star outline img location
+        this.outlineLayer.src = "./media/starFrame6.png";//gives the star outline img location
         //this.graffitiLayer.style.zIndex = 3;
 
         // Put it all together
